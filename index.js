28b985d0cbf2b1e87370fde21943ac080c8b7538
char502 <char502@hotmail.com>
2018-03-09 21:06:59 +0000

    Ajax and Jquery

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-    $('.question').on('mouseover', function () {
-        $('.answer').html('jQuery is the best thing in the world!');
-    });
+    var url = 'https://hn.algolia.com/api/v1/search?query=javascript';
+    $.getJSON(url, function (data) {
+
+        var currentQuote = '';
+        var quotes = data.hits;
+
+        $('.read').on('click', function () {
+            currentQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+            $('.quoteBody').html(currentQuote.title);
+            $('quoteAuthor').html(currentQuote.author);
+            // console.log(currentQuote);
+        });
+
+        // console.log(quotes[0].title);
+
+        console.log(data);
+    })
 });
